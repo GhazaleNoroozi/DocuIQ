d78d6c96e56401ed9b58f28b164d980d802079d7
Ghazaleh Noroozi <ghazalnoroozi27@yahoo.com>
2026-07-19 22:28:11 -0400

    accept uploading pdf in backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import cors from "cors";
+import multer from "multer";
+
 
 const app = express();
+const upload = multer({ dest: "uploads/" });
 
 app.use(cors());
 app.use(express.json());
@@ -9,6 +12,14 @@ app.use(express.json());
 app.get("/api/message", (req, res) => {
     res.json({
         message: "Hello from the backend!"
+    });
+});
+
+app.post("/api/documents", upload.single("file"), (req, res) => {
+    console.log(req.file);
+
+    res.json({
+        message: "File uploaded"
     });
 });
 
